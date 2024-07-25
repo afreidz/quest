f0b95a805e7f27c805f8a3f10ad659af0e7ea223
Andy Freidenfelds <afreidenfelds@hitachisolutions.com>
2024-07-25 14:06:22 -0500

    absolute basic getAll action for clients. to troubleshoot

--- src/actions/clients.ts
+++ src/actions/clients.ts
@@ -17,24 +17,9 @@ const schema = z.object({
 
 export const getAll = defineAction({
   input: PaginationSchema,
-  handler: async (pagination) => {
-    console.log("ANDY", "getting all clients...");
-
-    const clients = await orm.client
-      .findMany({
-        orderBy: { createdAt: "asc" },
-        // take: pagination?.take,
-        // skip: pagination?.skip,
-        // include,
-      })
-      .catch((err) => {
-        console.error(err);
-        return [];
-      });
-
-    console.log("ANDY", clients);
-
-    return clients;
+  handler: async () => {
+    console.log("HERE");
+    return [];
   },
 });
 
